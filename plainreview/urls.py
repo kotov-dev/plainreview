ad0e19394b7f446071c70e76740c87205ccd7fd1
Sergey Kotov <ikoshak@gmail.com>
2014-09-11 14:32:43 +0300

    static and templates initial

--- plainreview/urls.py
+++ plainreview/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
 
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'plainreview.views.home', name='home'),
+    url(r'^$', TemplateView.as_view(template_name="layout.html"), name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
